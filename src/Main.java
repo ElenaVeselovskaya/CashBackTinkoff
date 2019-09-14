a4e2138580d6f39a85edef7d441de84ec82f48e2
Student <student@itpark.ru>
2019-09-14 15:05:16 +0300

    Logic without limit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        int amountInRegular = 100;
+        int amountInIncreased = 1000;
+        int amountInSpecial = 10_000;
 
+        int percentInRegular = 1;
+        int percentInIncreased = 5;
+        int percentInSpecial = 30;
+
+        int result = (amountInRegular * percentInRegular +
+                amountInIncreased * percentInIncreased +
+                amountInSpecial * percentInSpecial) / 100;
+        System.out.println(result);
     }
 }
